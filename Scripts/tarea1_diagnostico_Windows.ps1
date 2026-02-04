d2f061e0bbc0ef2631898115f81d8197512c2b9f
Chonsi <murolopezjosue48@gmail.com>
2026-02-03 17:22:49 -0700

    Script Windows

--- Scripts/tarea1_diagnostico_Windows.ps1
+++ Scripts/tarea1_diagnostico_Windows.ps1
@@ -1,1 +1,10 @@
+Write-Host "===== DIAGNOSTICO DEL SISTEMA ====="
 
+Write-Host "Nombre del equipo:"
+hostname
+
+Write-Host "`nDirecciones IP:"
+Get-NetIPAddress | Where-Object {$_.AddressFamily -eq "IPv4"}
+
+Write-Host "`nEspacio en disco:"
+Get-PSDrive C
